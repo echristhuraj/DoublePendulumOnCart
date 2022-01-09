80ee8c271a3ccab7baf5923c9230de5e1d4f625e
Edwin Christhuraj <85334364+echristhuraj@users.noreply.github.com>
2022-01-08 20:35:17 -0800

    Created Main file
    Signed-off-by: Edwin Christhuraj <85334364+echristhuraj@users.noreply.github.com>

---
+++ Main.m
@@ -0,0 +1,12 @@
+% Filename:      Main.m
+%
+% Description:   Runs a program that simulates the dynamics of a 3DOF
+%                double pendulum-sliding cart system using the state space
+%                representation with linearization about four fixed points.
+%                Friction is neglected.
+%
+% Author(s):     Edwin Christhuraj
+% Created:       23-Apr-2021
+%
+% Copyright (c) 2022 Edwin Christhuraj.
+% See LICENSE for terms.
