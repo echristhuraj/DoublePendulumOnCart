7152d56e22153311747c19e02f5a919756e447b0
Edwin Christhuraj <85334364+echristhuraj@users.noreply.github.com>
2022-01-10 00:12:13 -0800

    Matlab figure object for plot created

--- src/Main.m
+++ src/Main.m
@@ -8,7 +8,25 @@
 %                system is controllable. Friction is neglected.
 %
 % Author(s):     Edwin Christhuraj
-% Created:       23-Apr-2021
+% Created:       8-Jan-2022
 %
 % Copyright (c) 2022 Edwin Christhuraj.
 % See LICENSE for terms.
+
+clear;
+
+%% Main Parameters
+
+
+%% Main Setup
+comp_scrn_size = get(0, 'ScreenSize');
+simUI = figure('Name', 'Double Pendulum On Cart', 'Position', [comp_scrn_size(3) / 4, comp_scrn_size(4) / 4, 0.5 * comp_scrn_size(3), 0.5 * comp_scrn_size(4)], 'Color', 'k');
+panel = uipanel('Parent', simUI);
+set(panel, 'BackgroundColor', 'k');
+axes = axes('Parent', panel);
+set(axes, 'Visible', 'on', 'Color', 'k', 'xcolor', 'w', 'ycolor', 'w', 'zcolor', 'w');
+hold(axes, 'on');
+hold(axes, 'off');
+
+%% Main Simulation
+
