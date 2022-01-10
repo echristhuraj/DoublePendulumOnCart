e9f7ae1daba0bcb67e650d2ae72ae31152651c68
Edwin Christhuraj <85334364+echristhuraj@users.noreply.github.com>
2022-01-09 21:10:17 -0800

    Modified Main.m file description

--- Main.m
+++ Main.m
@@ -1,9 +1,11 @@
 % Filename:      Main.m
 %
-% Description:   Runs a program that simulates the dynamics of a 3DOF
-%                double pendulum-sliding cart system using the state space
-%                representation with linearization about four fixed points.
-%                Friction is neglected.
+% Description:   Runs a program that simulates the dynamics and control of
+%                a 3DOF double pendulum-sliding cart system. The system is
+%                under single-axis control (of the cart's horizontal
+%                position) and uses the state space representation with
+%                linearization about four fixed points near which the
+%                system is controllable. Friction is neglected.
 %
 % Author(s):     Edwin Christhuraj
 % Created:       23-Apr-2021
